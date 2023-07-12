1d00d50329648fa7e3349896077ba52e54bedb70
Tim Xie <timxie140@gmail.com>
2023-07-12 13:00:11 -0400

    add gettemperature handler

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"io/ioutil"
 	"github.com/gorilla/mux"
+	
 	"net/http"
 	"encoding/json"
 	"log"
 )
 
+var sensorList = []temp_sensor{}
+
 type temp_sensor struct {
 	ID string `json:"id"`
-	Name string `json:"name"`
 	Room string `json:"room"`
+	Temperature int `json:"temperature"`
 }
 
 func addTempSensorHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +24,8 @@ func addTempSensorHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Can't read body", http.StatusBadRequest)
 		return
 	}
-	newSensor := temp_sensor{}
+	newSensor := temp_sensor{"A", "", 34}
+	sensorList = append(sensorList, newSensor)
 	json.Unmarshal(body, &newSensor)
 	fmt.Printf("Adding new sensor %s\n", newSensor.ID)
 	fmt.Fprintf(w, "Sensor %s added\n", newSensor.ID)
@@ -33,16 +37,29 @@ func deleteTempSensorHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Can't read body", http.StatusBadRequest) 
 		return
 	}
-	delSensor := temp_sensor{}
+	delSensor := temp_sensor{"B", "", 22}
+
 	json.Unmarshal(body, &delSensor)
 	fmt.Printf("Deleting sensor %s\n", delSensor.ID)
 	fmt.Fprintf(w, "Sensor %s deleted\n", delSensor.ID)
+}
+
+func getTemperatureHandler (w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	sensorID := vars["id"]
+	for _, sensor := range sensorList {
+		if sensor.ID == sensorID {
+			fmt.Fprint(w, sensor.Temperature)
+			return
+		}
+	}
 }
 
 func handleRequest() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/sensor/temperature", addTempSensorHandler).Methods("POST")
 	router.HandleFunc("/sensor/temperature", deleteTempSensorHandler).Methods("DELETE")
+	router.HandleFunc("/sensor/temperature/{id}", getTemperatureHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":5001", router))
 }
